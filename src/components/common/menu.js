70b37b0ecd467c9471b8523d1a660613a0943e8d
kanekotic <alvarojosepl@gmail.com>
2018-03-08 08:53:44 +0100

    added ui handling

--- src/components/common/menu.js
+++ src/components/common/menu.js
@@ -1,25 +1,28 @@
-
 import React, { Component } from 'react'
 import { Actions } from 'react-native-router-flux'
-
 import { Fab, Icon, Button } from 'native-base'
 import { connect } from 'react-redux'
+import ui from 'redux-ui'
+
+const uiScope = { 
+    key: 'fav-menu', 
+    state: { active : false }
+}
 
 class Menu extends Component {
-    constructor(props) {
-        super(props)
-    }
-
     render(){
         return(
             <Fab
-                active={this.props.active}
+                active={this.props.ui.active}
                 direction="up"
                 position="bottomRight"
-                onPress={() =>{}}>
+                onPress={() =>this.props.updateUI('active', !this.props.ui.active)}>
                 <Icon name="md-more" />
                 <Button>
-                    <Icon name="md-settings" onPress={ () =>{}} />
+                    <Icon name="md-infinite" onPress={ () =>{
+                        console.log("do something else")
+                        Actions.refresh()
+                        }} />
                 </Button>
           </Fab>
         )
@@ -30,4 +33,4 @@ function mapStateToProps(state){
     return {}
 }
 
-export default connect(mapStateToProps)(Menu)
+export default connect(mapStateToProps)(ui(uiScope)(Menu))
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import { createReducer } from 'redux-create-reducer'
+import { reducer as uiReducer } from 'redux-ui'
 
 let emptyReducer = createReducer(false , () => {return false})
 
-export default combineReducers({emptyReducer})
+export default combineReducers({emptyReducer, ui: uiReducer})
